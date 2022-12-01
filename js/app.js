251fc29012b371017d595bd85723813480f80c81
arsenlenovo <arsenarsarsenars85@gmail.com>
2022-12-01 20:19:41 +0600

    window

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// 3D scroll
+
+let zSpacing = -1000,
+ lastPos = zPacing / 5
+ $frames = document.getElementsByClassName('frame'),
+ frame = Array.from($frames), 
+ zVals = []
+
+
+
+
+ window.onscroll = fucntion() {
+ let top = document.documentElement.scrollTop,
+     delta = lastPos - top
+
+ lastPos = top
+ 
+ frames.forEach(fucntion(n, i) {
+  zVals.
+ })
+ }
